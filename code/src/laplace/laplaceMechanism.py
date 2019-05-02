6d303a13ffabb932818a87a120d95d95cd67c949
Davi Grossi Hasuda <davigrossihasuda@MacBook-Pro-de-Davi.local>
2019-05-02 00:26:44 -0300

    Adding laplace mechanism

--- code/src/laplace/laplaceMechanism.py
+++ code/src/laplace/laplaceMechanism.py
@@ -1,6 +1,17 @@
 import numpy as np
 
-print('Laplace mechanism')
-mean, scale = 0., 1.
-s = np.random.laplace(mean, scale, 1)[0]
-print(s)
+class LaplacianMethod:
+  _mean, _scale = 0., 1.
+  def __init__(self, mean = 0., scale = 1.):
+    self._mean = mean
+    self._scale = scale
+
+  def generatePrivateAnswer(self, truth):
+    sanitizedTruth = 0
+    try:
+      sanitizedTruth = float(truth)
+    except:
+      return 0
+    noise = np.random.laplace(mean, scale, 1)[0]
+    return sanitizedTruth + noise
+  
