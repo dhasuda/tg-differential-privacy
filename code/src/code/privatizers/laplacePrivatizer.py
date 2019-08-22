3e0f2a64bb3a0b12ea7eeb95cc357dcfbbc57cd5
dhasuda <davihasuda@gmail.com>
2019-08-22 17:00:01 -0300

    Adding Laplace Privatizer

---
+++ code/src/code/privatizers/laplacePrivatizer.py
@@ -0,0 +1,31 @@
+import numpy as np
+
+class LaplacePrivatizer:
+  _mean = 0.0
+  _scale = 1.0
+
+  def __init__(self, scale=1.):
+    if (type(scale) != float):
+      raise ValueError('Not a valid scale')
+    if (scale <= 0.0):
+      raise ValueError('Not a valid scale')
+    self._scale = scale
+
+  def privatizeList(self, data):
+    if (type(data) != list):
+      raise ValueError('Not a list')
+    
+    privatizedList = []
+    for value in data:
+      privateValue = self.privatizeSingleAnswer(value)
+      privatizedList.append(privateValue)
+    return privatizedList
+
+  def privatizeSingleAnswer(self, truth):
+    sanitizedTruth = 0
+    try:
+      sanitizedTruth = float(truth)
+    except:
+      raise ValueError('Not valid value to be privatized')
+    noise = np.random.laplace(self._mean, self._scale, 1)[0]
+    return float(sanitizedTruth + noise)
---
+++ code/src/code/privatizers/laplaceTests.py
@@ -0,0 +1,51 @@
+import unittest
+import laplacePrivatizer
+
+class LaplacePrivatizerTests(unittest.TestCase):
+
+  def testInitializer(self):
+    with self.assertRaises(ValueError) as stringContext:
+      laplacePrivatizer.LaplacePrivatizer('a')
+    self.assertTrue('Not a valid scale' in stringContext.exception)
+
+    with self.assertRaises(ValueError) as negativeContext:
+      laplacePrivatizer.LaplacePrivatizer(0.)
+    self.assertTrue('Not a valid scale' in negativeContext.exception)
+
+    with self.assertRaises(ValueError) as zeroContext:
+      laplacePrivatizer.LaplacePrivatizer(-1.)
+    self.assertTrue('Not a valid scale' in zeroContext.exception)
+
+    priv = laplacePrivatizer.LaplacePrivatizer()
+    self.assertEqual(1., priv._scale)
+
+    priv = laplacePrivatizer.LaplacePrivatizer(0.5)
+    self.assertEqual(0.5, priv._scale)
+
+  def testPrivatizeSingleAnswer(self):
+    priv = laplacePrivatizer.LaplacePrivatizer()
+    with self.assertRaises(ValueError) as context:
+      priv.privatizeSingleAnswer('a')
+    self.assertTrue('Not valid value to be privatized' in context.exception)
+
+    answer = priv.privatizeSingleAnswer(1.0)
+    self.assertTrue(type(answer) == float)
+
+  def testPrivatizeList(self):
+    priv = laplacePrivatizer.LaplacePrivatizer()
+
+    with self.assertRaises(ValueError) as context:
+      priv.privatizeList(1.0)
+    self.assertTrue('Not a list' in context.exception)
+
+    emptyList = priv.privatizeList([])
+    self.assertEqual([], emptyList)
+
+    notEmptyList = priv.privatizeList([-1., 0., 1., 2.])
+    self.assertEqual(len(notEmptyList), len([-1., 0., 1., 2.]))
+    for value in notEmptyList:
+      self.assertTrue(type(value) == float)
+
+
+if __name__ == "__main__":
+  unittest.main()
