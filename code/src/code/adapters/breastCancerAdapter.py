827a475e5dd66403da818978e6f9d7e80b47697c
dhasuda <davihasuda@gmail.com>
2019-08-28 22:34:57 -0300

    Ading adapter to notbook

--- code/src/code/adapters/breastCancerAdapter.py
+++ code/src/code/adapters/breastCancerAdapter.py
@@ -6,11 +6,31 @@ def fromRaw(rawData):
 
   adaptedData = []
   for data in rawData:
-    if (type(data) != np.ndarray):
-      raise ValueError('Raw data values must be numpy ndarrays')
     floatList = []
-    for value in data:
-      floatList.append(float(value))
-    adaptedData.append(list(floatList))
+    if (type(data) == np.ndarray):
+      for value in data:
+        floatList.append(float(value))
+      adaptedData.append(list(floatList))
+    else:
+      adaptedData.append(toFloat(data))
 
   return adaptedData
+
+def toFloat(value):
+  try:
+    return float(value)
+  except:
+    raise ValueError('Cannot parse to float')
+
+def toBinaryInt(data):
+  if type(data) == list:
+    intList = []
+    for value in data:
+      intList.append(toBinaryInt(value))
+    return intList
+  elif type(data) == float:
+    if (data >= 0.5):
+      return 1
+    return 0
+  else:
+    raise ValueError('It only accepts lists of float values')
--- code/src/code/adapters/breastCancerTests.py
+++ code/src/code/adapters/breastCancerTests.py
@@ -15,5 +15,12 @@ class BreastCancerAdapterTests(unittest.TestCase):
     self.assertEqual(list, type(output[0]))
     self.assertEqual(float, type(output[0][0]))
 
+  def testToIntList(self):
+    values = 0.1
+    self.assertEqual(0, adapter.toBinaryInt(values))
+
+    values = [0.1, 0.5, 0.6, 1.1, -0.1]
+    self.assertEqual([0, 1, 1, 1, 0], adapter.toBinaryInt(values))
+
 if __name__ == "__main__":
   unittest.main()
